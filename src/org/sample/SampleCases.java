70cb8bc53c2013c8894e5abc200afbc7dcb013ef
Jenselens <jenselens27@gmail.com>
2021-07-24 10:18:21 +0530

    Message

---
+++ src/org/sample/SampleCases.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class SampleCases {
+	public SampleCases() {
+	}
+	public void test() {
+		System.out.println("1");
+	}
+
+	  public static final void main(String[] args) {
+		SampleCases cases = new SampleCases();
+		cases.test();
+	}
+}
